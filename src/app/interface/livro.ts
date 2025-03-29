3f5c2b6d1098c07c56637d383491194f45c2f372
iagoponte <iago.ponte.01@gmail.com>
2025-03-29 13:52:16 -0300

    feat: interface - Livro e updateLivro_Services

---
+++ src/app/interface/livro.ts
@@ -0,0 +1,9 @@
+export interface Livro {
+    id: number,
+    titulo: string,
+    ano_publicacao: number,
+    genero: string,
+    capa: string,
+    sinopse: string,
+    preco: number
+}
--- src/app/services/livros.service.ts
+++ src/app/services/livros.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { map, Observable } from 'rxjs';
+import { Livro } from '../interface/livro';
 
 @Injectable({
   providedIn: 'root',
@@ -10,20 +11,23 @@ export class LivrosService {
   constructor(private httpClient: HttpClient) {}
   private apiUrl = `${environment.apiUrl}/livros`;
 
-  getLivros(): Observable<any> {
-    return this.httpClient.get(this.apiUrl).pipe(
-      map((response) => {
-        return response;
-      })
-    );
+  getLivros(): Observable<Livro[]> {
+    return this.httpClient.get<Livro[]>(this.apiUrl)
+    // .pipe(
+    //   map((response) => {
+    //     return response;
+    //   })
+    // );
+    // talvez, não usar o pipe aqui, poderá fazer com que eu não consiga obter o retorno do meu back, por conta de como estruturei o json das respostas.
   }
 
-  getLivro(id: number): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/${id}`).pipe(
-      map((response) => {
-        return response;
-      })
-    );
+  getLivro(id: number): Observable<Livro> {
+    return this.httpClient.get<Livro>(`${this.apiUrl}/${id}`)
+    // .pipe(
+    //   map((response) => {
+    //     return response;
+    //   })
+    // );
   }
 
   //o correto seria criar um interface para o livro. Caso contrário, o livro pode ser qualquer coisa.
@@ -33,5 +37,11 @@ export class LivrosService {
         return response;
       })
     );
+  }
+
+  updateLivro(id: number, livro: any): Observable<any> {
+    return this.httpClient.put(`${this.apiUrl}/atualizar/${id}`, livro).pipe(map((response) => {
+      return response;
+    }))
   }
 }
